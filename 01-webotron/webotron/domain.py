56617fec1d0e3c2b10b813dcd94937433dd9ca7e
TokenRing <tiadevops@gmail.com>
2020-06-11 10:48:01 -0700

    Set up Cloud Front CDN and SSL

--- 01-webotron/webotron/domain.py
+++ 01-webotron/webotron/domain.py
@@ -6,16 +6,13 @@ import uuid
 
 
 class DomainManager:
-    """Manage Route 53 domain."""
+    """Manage a Route 53 domain."""
 
     def __init__(self, session):
         """Create DomainManager object."""
         self.session = session
         self.client = self.session.client('route53')
 
-    # hosted zone end with a "." 'tokenring.net.'
-    # domain name will be 'kittentest.tokenring.net'
-    # it can be even longer: 'subdomain.kittentest.tokenring.net'
     def find_hosted_zone(self, domain_name):
         """Find zone matching domain_name."""
         paginator = self.client.get_paginator('list_hosted_zones')
@@ -24,40 +21,56 @@ class DomainManager:
                 if domain_name.endswith(zone['Name'][:-1]):
                     return zone
 
-        # we'll either get a zone matching our domain or we'll get None
         return None
 
-    # we need to go from a string like this:
-    # domain_name = 'subdomain.kittentest.tokenring.net'
-    # to a string like this:
-    # zone_name = 'tokenring.net.'
     def create_hosted_zone(self, domain_name):
         """Create a hosted zone to match domain_name."""
-        zone_name = ".".join(domain_name.split('.')[-2:]) + '.'
+        zone_name = '.'.join(domain_name.split('.')[-2:]) + '.'
         return self.client.create_hosted_zone(
-                Name=zone_name,
-                CallerReference=str(uuid.uuid4())
+            Name=zone_name,
+            CallerReference=str(uuid.uuid4())
         )
 
     def create_s3_domain_record(self, zone, domain_name, endpoint):
         """Create a domain record in zone for domain_name."""
         return self.client.change_resource_record_sets(
-                HostedZoneId=zone['Id'],
-                ChangeBatch={
-                    'Comment': 'Created by webotron',
-                    'Changes': [{
-                            'Action': 'UPSERT',
-                            'ResourceRecordSet': {
-                                'Name': domain_name,
-                                'Type': 'A',
-                                'AliasTarget': {
-                                    'HostedZoneId': endpoint.zone,
-                                    'DNSName': endpoint.host,
-                                    'EvaluateTargetHealth': False
-                                 },
-
+            HostedZoneId=zone['Id'],
+            ChangeBatch={
+                'Comment': 'Created by webotron',
+                'Changes': [{
+                        'Action': 'UPSERT',
+                        'ResourceRecordSet': {
+                            'Name': domain_name,
+                            'Type': 'A',
+                            'AliasTarget': {
+                                'HostedZoneId': endpoint.zone,
+                                'DNSName': endpoint.host,
+                                'EvaluateTargetHealth': False
                             }
+                        }
+                    }
+                ]
+            }
+        )
 
-                      }]
-                }
+    def create_cf_domain_record(self, zone, domain_name, cf_domain):
+        """Create a domain record in zone for domain_name."""
+        return self.client.change_resource_record_sets(
+            HostedZoneId=zone['Id'],
+            ChangeBatch={
+                'Comment': 'Created by webotron',
+                'Changes': [{
+                        'Action': 'UPSERT',
+                        'ResourceRecordSet': {
+                            'Name': domain_name,
+                            'Type': 'A',
+                            'AliasTarget': {
+                                'HostedZoneId': 'Z2FDTNDATAQYW2',
+                                'DNSName': cf_domain,
+                                'EvaluateTargetHealth': False
+                            }
+                        }
+                    }
+                ]
+            }
         )
--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,15 +1,14 @@
-#!/c/Python38/python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 """Webotron: Deploy websites with aws.
-
-Webotron automates the process of deploying statis websites
-- Configure AWS S3 bucket
-    - Create them
-    - Set them up for statis web hosting
-    - Deploy local files to them
+Webotron automates the process of deploying static websites to AWS.
+- Configure AWS S3 buckets
+  - Create them
+  - Set them up for static website hosting
+  - Deploy local files to them
 - Configure DNS with AWS Route 53
-- Configure a Content Delivery Network and SSL with AWS
+- Configure a Content Delivery Network and SSL with AWS CloudFront
 """
 
 import boto3
@@ -17,19 +16,25 @@ import click
 
 from bucket import BucketManager
 from domain import DomainManager
+from certificate import CertificateManager
+from cdn import DistributionManager
+
 import util
 
 session = None
 bucket_manager = None
-domain_Manager = None
+domain_manager = None
+cert_manager = None
+dist_manager = None
 
 
 @click.group()
 @click.option('--profile', default=None,
-        help="Use a given AWS profile.")
+              help="Use a given AWS profile.")
 def cli(profile):
     """Webotron deploys websites to AWS."""
-    global session, bucket_manager, domain_manager
+    global session, bucket_manager, domain_manager, cert_manager, dist_manager
+
     session_cfg = {}
     if profile:
         session_cfg['profile_name'] = profile
@@ -37,6 +42,8 @@ def cli(profile):
     session = boto3.Session(**session_cfg)
     bucket_manager = BucketManager(session)
     domain_manager = DomainManager(session)
+    cert_manager = CertificateManager(session)
+    dist_manager = DistributionManager(session)
 
 
 @cli.command('list-buckets')
@@ -49,7 +56,7 @@ def list_buckets():
 @cli.command('list-bucket-objects')
 @click.argument('bucket')
 def list_bucket_objects(bucket):
-    """List objects in a s3 bucket."""
+    """List objects in an s3 bucket."""
     for obj in bucket_manager.all_objects(bucket):
         print(obj)
 
@@ -69,7 +76,7 @@ def setup_bucket(bucket):
 @click.argument('pathname', type=click.Path(exists=True))
 @click.argument('bucket')
 def sync(pathname, bucket):
-    """Sync content of PATHNAME to BUCKET."""
+    """Sync contents of PATHNAME to BUCKET."""
     bucket_manager.sync(pathname, bucket)
     print(bucket_manager.get_bucket_url(bucket_manager.s3.Bucket(bucket)))
 
@@ -81,14 +88,44 @@ def setup_domain(domain):
     bucket = bucket_manager.get_bucket(domain)
 
     zone = domain_manager.find_hosted_zone(domain) \
-            or domain_manager.create_hosted_zone(domain)
+        or domain_manager.create_hosted_zone(domain)
 
     endpoint = util.get_endpoint(bucket_manager.get_region_name(bucket))
-    a_record = domain_manager.create_s3_domain_record(zone, domain, endpoint)
-    # for this to work the bucket name should match the domain we are deplying to
-    print("Domain configure: http://{}".format(domain))
+    domain_manager.create_s3_domain_record(zone, domain, endpoint)
+    print("Domain configured: http://{}".format(domain))
 
-    print(a_record)
+
+@cli.command('find-cert')
+@click.argument('domain')
+def find_cert(domain):
+    """Find a certificate for <DOMAIN>."""
+    print(cert_manager.find_matching_cert(domain))
+
+
+@cli.command('setup-cdn')
+@click.argument('domain')
+@click.argument('bucket')
+def setup_cdn(domain, bucket):
+    """Set up CloudFront CDN for DOMAIN pointing to BUCKET."""
+    dist = dist_manager.find_matching_dist(domain)
+
+    if not dist:
+        cert = cert_manager.find_matching_cert(domain)
+        if not cert:  # SSL is not optional at this time
+            print("Error: No matching cert found.")
+            return
+
+        dist = dist_manager.create_dist(domain, cert)
+        print("Waiting for distribution deployment...")
+        dist_manager.await_deploy(dist)
+
+    zone = domain_manager.find_hosted_zone(domain) \
+        or domain_manager.create_hosted_zone(domain)
+
+    domain_manager.create_cf_domain_record(zone, domain, dist['DomainName'])
+    print("Domain configured: https://{}".format(domain))
+
+    return
 
 
 if __name__ == '__main__':
