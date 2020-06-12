35fff83a0343991cb461e385c702e5d1b70ae906
TokenRing <tiadevops@gmail.com>
2020-06-12 14:23:24 -0700

    Add example of posting to slack webhook

---
+++ 02-notifon/post_to_slack.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+# url = 'https://hooks.slack.com/services/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'
+# need to replace the URL with slack's webhook
+data = { "text": "Hello, world." }
+requests.post(url, json=data)
+import requests
+requests.post(url, json=data)
